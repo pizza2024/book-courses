f6761ec2ed3f1c0d0f60eccdf33d29893b44f5a5
tzy <tangzy2021@gmail.com>
2024-06-14 18:55:24 +0800

    feat: 建表语句

--- packages/server/initdb.ts
+++ packages/server/initdb.ts
@@ -1,3 +1,5 @@
+import { forEach } from 'lodash';
+import moment from 'moment';
 import mysql from 'mysql2';
 import pool from "./src/db/connection-pool";
 const dropTables = async () => {
@@ -62,7 +64,6 @@ const createTables = async () => {
     await connection.query(`create table if not exists course (
       id int NOT NULL AUTO_INCREMENT,
       name varchar(50) NOT NULL UNIQUE,
-      room varchar(256) NOT NULL,
       adminId int,
       PRIMARY KEY (id),
       FOREIGN KEY (adminId) REFERENCES admin(id)
@@ -77,8 +78,6 @@ const createTables = async () => {
       repeatDay varchar(7),
       isOpen boolean,
       canJoin boolean,
-      studentCount int,
-      studentLimit int,
       adminId int,
       PRIMARY KEY (courseId, teacherId),
       FOREIGN KEY (adminId) REFERENCES admin(id),
@@ -107,18 +106,181 @@ const createTables = async () => {
   }
 }
 const insertDatas = async () => {
-  const admins = [
-    { username: 'admin1', password: mysql.raw('SHA2("123456", 256)'), adminRoleId: 1 },
-    { username: 'admin2', password: mysql.raw('SHA2("123456", 256)'), adminRoleId: 2 },
-    { username: 'admin3', password: mysql.raw('SHA2("123456", 256)'), adminRoleId: 2 }
-  ]
+  const dataMap = {
+    adminRole: [
+      {
+        name: 'super',
+      },
+      {
+        name: 'normal',
+      }
+    ],
+    admin: [
+      { username: 'admin1', password: mysql.raw('SHA2("123456", 256)'), adminRoleId: 1 },
+      { username: 'admin2', password: mysql.raw('SHA2("123456", 256)'), adminRoleId: 2 },
+      { username: 'admin3', password: mysql.raw('SHA2("123456", 256)'), adminRoleId: 2 }
+    ],
+    teacher: [
+      { username: 'teacher1', password: mysql.raw('SHA2("123456", 256)'), adminId: 1 },
+      { username: 'teacher2', password: mysql.raw('SHA2("123456", 256)'), adminId: 1 },
+      { username: 'teacher3', password: mysql.raw('SHA2("123456", 256)'), adminId: 2 },
+      { username: 'teacher4', password: mysql.raw('SHA2("123456", 256)'), adminId: 3 }
+    ],
+    student: [
+      { username: 'student1', password: mysql.raw('SHA2("123456", 256)'), adminId: 1 },
+      { username: 'student2', password: mysql.raw('SHA2("123456", 256)'), adminId: 1 },
+      { username: 'student3', password: mysql.raw('SHA2("123456", 256)'), adminId: 2 },
+      { username: 'student4', password: mysql.raw('SHA2("123456", 256)'), adminId: 3 },
+      { username: 'student5', password: mysql.raw('SHA2("123456", 256)'), adminId: 3 }
+    ],
+    course: [
+      { name: '音乐', adminId: 1 },
+      { name: '美术', adminId: 1 },
+      { name: '数学', adminId: 1 },
+      { name: '体育', adminId: 1 },
+      { name: '语文', adminId: 1 },
+      { name: '英语', adminId: 1 },
+      { name: '化学', adminId: 1 },
+      { name: '政治', adminId: 1 },
+      { name: '物理', adminId: 1 },
+      { name: '自习', adminId: 1 },
+    ],
+    publishedCourse: [
+      {
+        courseId: 1,
+        teacherId: 1,
+        classRoom: 'classRoom1',
+        daytime: JSON.stringify([
+          { startTime: '08:00:00', endTime: '08:45:00' },
+          { startTime: '18:00:00', endTime: '18:45:00' }
+        ]),
+        launchTime: moment(new Date('2024-03-01')).format('YYYY-MM-DD HH:mm:ss'),
+        removeTime: moment(new Date('2024-08-01')).format('YYYY-MM-DD HH:mm:ss'),
+        repeatDay: '1111100',
+        isOpen: true,
+        canJoin: true,
+        adminId: 1
+      },
+      {
+        courseId: 1,
+        teacherId: 2,
+        classRoom: 'classRoom1',
+        daytime: JSON.stringify([
+          { startTime: '08:00:00', endTime: '08:45:00' },
+          { startTime: '18:00:00', endTime: '18:45:00' }
+        ]),
+        launchTime: moment(new Date('2024-03-01')).format('YYYY-MM-DD HH:mm:ss'),
+        removeTime: moment(new Date('2024-08-01')).format('YYYY-MM-DD HH:mm:ss'),
+        repeatDay: '1111100',
+        isOpen: true,
+        canJoin: true,
+        adminId: 1
+      },
+      {
+        courseId: 2,
+        teacherId: 1,
+        classRoom: 'classRoom2',
+        daytime: JSON.stringify([
+          { startTime: '08:00:00', endTime: '08:45:00' },
+          { startTime: '18:00:00', endTime: '18:45:00' }
+        ]),
+        launchTime: moment(new Date('2024-03-01')).format('YYYY-MM-DD HH:mm:ss'),
+        removeTime: moment(new Date('2024-08-01')).format('YYYY-MM-DD HH:mm:ss'),
+        repeatDay: '1111100',
+        isOpen: true,
+        canJoin: true,
+        adminId: 1
+      },
+      {
+        courseId: 3,
+        teacherId: 2,
+        classRoom: 'classRoom3',
+        daytime: JSON.stringify([
+          { startTime: '08:00:00', endTime: '08:45:00' },
+          { startTime: '18:00:00', endTime: '18:45:00' }
+        ]),
+        launchTime: moment(new Date('2024-03-01')).format('YYYY-MM-DD HH:mm:ss'),
+        removeTime: moment(new Date('2024-08-01')).format('YYYY-MM-DD HH:mm:ss'),
+        repeatDay: '1111100',
+        isOpen: true,
+        canJoin: true,
+        adminId: 1
+      },
+      {
+        courseId: 4,
+        teacherId: 3,
+        classRoom: 'classRoom4',
+        daytime: JSON.stringify([
+          { startTime: '08:00:00', endTime: '08:45:00' },
+          { startTime: '18:00:00', endTime: '18:45:00' }
+        ]),
+        launchTime: moment(new Date('2024-03-01')).format('YYYY-MM-DD HH:mm:ss'),
+        removeTime: moment(new Date('2024-08-01')).format('YYYY-MM-DD HH:mm:ss'),
+        repeatDay: '1111100',
+        isOpen: true,
+        canJoin: true,
+        adminId: 1
+      }
+    ],
+    bookedCourse: [
+      {
+        courseId: 1,
+        teacherId: 1,
+        studentId: 1,
+        signIn: false,
+        askLeave: false,
+        adminId: 1
+      },
+      {
+        courseId: 3,
+        teacherId: 2,
+        studentId: 1,
+        signIn: false,
+        askLeave: false,
+        adminId: 1
+      },
+      {
+        courseId: 3,
+        teacherId: 2,
+        studentId: 3,
+        signIn: false,
+        askLeave: false,
+        adminId: 1
+      },
+      {
+        courseId: 4,
+        teacherId: 3,
+        studentId: 1,
+        signIn: false,
+        askLeave: false,
+        adminId: 1
+      },
+      {
+        courseId: 4,
+        teacherId: 3,
+        studentId: 5,
+        signIn: false,
+        askLeave: false,
+        adminId: 1
+      },
+      {
+        courseId: 4,
+        teacherId: 3,
+        studentId: 3,
+        signIn: false,
+        askLeave: false,
+        adminId: 1
+      },
+    ]
+  }
   const connection = await pool.getConnection();
   await connection.beginTransaction();
   try {
-    await connection.query(`insert into adminRole values (null, 'super'), (null, 'normal')`)
-    for (let admin of admins) {
-      await connection.query(`insert into admin set ?`, admin);
-    }
+    forEach(dataMap, async (rows, tableName) => {
+      forEach(rows, async (row) => {
+        await connection.query(`insert into ${tableName} set ?`, row);
+      })
+    })
     await connection.commit();
     await connection.release();
   } catch (e) {
